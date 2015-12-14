67122fcf2b1d939dca5f39e9cd27915a43f9bd08
Maikel <maikeldus@hotmail.com>
2015-12-14 23:53:44 +0100

    Upgrade to 1.6.0

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ angular.module('whatsspy', ['ngRoute', 'ngVis', 'whatsspyFilters', 'whatsspyCont
 })
 .controller('MainController', function($scope, $rootScope, $location, $timeout, $http, $q, $filter, $sce, $translate) {
   // Version of the application
-  $rootScope.version = '1.5.9';
+  $rootScope.version = '1.6.0';
 
   $timeout(function() {
     $('[data-toggle="tooltip"]').tooltip();
